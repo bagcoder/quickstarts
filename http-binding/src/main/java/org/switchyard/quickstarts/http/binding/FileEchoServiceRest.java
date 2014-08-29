cd31ea605d79794715bb620d7f57d1bd40b3679a
mark <mlybarger@gmail.com>
2014-08-29 08:03:23 -0400

    change

--- http-binding/src/main/java/org/switchyard/quickstarts/http/binding/FileEchoServiceRest.java
+++ http-binding/src/main/java/org/switchyard/quickstarts/http/binding/FileEchoServiceRest.java
@@ -1,7 +1,5 @@
 package org.switchyard.quickstarts.http.binding;
 
-import java.io.InputStream;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -9,15 +7,11 @@ import javax.ws.rs.core.Context;
 
 @Path("/b")
 public interface FileEchoServiceRest {
-	@POST
-	@Path("/")
-	public void receiveBytesIS(InputStream is);
 
 	/**
 	*/
 	@POST
 	@Path("/d")
 	public void receiveBytes(@Context HttpServletRequest request);
-	
-}
 
+}
